91b0a9d5cf527b2690d667f38293cc55564c709a
Raymond Yepiz <93514024+RayYepiz@users.noreply.github.com>
2024-09-11 18:54:08 -0700

    updated the vite.config.js file.

--- promodoro/vite.config.js
+++ promodoro/vite.config.js
@@ -3,16 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  resolve: {
-    alias: {
-      "@": "/src",
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
   plugins: [react()],
 });
