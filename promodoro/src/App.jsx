8f9a9eec2933cf5fa4fa1fc4fdb7b84c2b2bc7d9
Raymond Yepiz <93514024+RayYepiz@users.noreply.github.com>
2024-09-26 20:50:32 -0700

    added styles to the application. using fun google fonts, and added a simple color palette. I will probably add a hover effect on the buttons and that should be it for this basic promodoro project.

--- promodoro/src/App.jsx
+++ promodoro/src/App.jsx
@@ -4,10 +4,20 @@ import Timer from "./components/Timer";
 
 function App() {
   return (
-    <div className="timersWrapper">
-      <Timer time={"25:00"} />
-      <Timer time={"05:00"} />
-    </div>
+    <>
+      <div className="wrapper">
+        <h1 className="header">Promodoro Timer</h1>
+        <div className="timersWrapper">
+          <div className="primaryTimer">
+            <Timer time={"25:00"} />
+          </div>
+
+          <div className="breakTimer">
+            <Timer time={"05:00"} />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
